e74026c143ea5a76c1652fede0ca592affc7e37c
Danielpba02 <pereyradaniel2020@gmail.com>
2022-03-06 18:22:29 -0400

    Suma de numeros

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Sumar dos numeros");
+
+int num = 3;
+int i = 0;
+Console.WriteLine(num + i);
+
+
